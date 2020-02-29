49abaa9cb07886abd8bcb2fc34d2a5e364848076
Samir Ferreira <svaf2@cin.ufpe.br>
2020-02-29 18:35:15 -0300

    Multer config and files upload feature

--- src/routes.js
+++ src/routes.js
@@ -2,18 +2,24 @@ import { Router } from 'express';
 
 import SessionController from './app/controllers/SessionController';
 import RecipientController from './app/controllers/RecipientController';
+import FileController from './app/controllers/FileController';
 
 import authMiddleware from './app/middlewares/authMiddleware';
 
 const routes = new Router();
 
+// Session login
 routes.post('/sessions', SessionController.store);
 
 // Auth verification
 routes.use(authMiddleware);
 
+// Recipients
 routes.get('/recipients', RecipientController.index);
 routes.post('/recipients', RecipientController.store);
 routes.put('/recipients/:id', RecipientController.update);
+
+// Files upload
+routes.post('/files', FileController.store);
 
 export default routes;
